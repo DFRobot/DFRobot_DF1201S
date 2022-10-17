38bdc726d79aadd79245c3a885ed356d787bc551
Nickman87 <nick.defrangh@gmail.com>
2022-10-17 16:17:04 +0200

    Update DFRobot_DF1201S.cpp
    Also set the pauseFlag when starting a specific file firectly

--- src/DFRobot_DF1201S.cpp
+++ src/DFRobot_DF1201S.cpp
@@ -208,6 +208,7 @@ bool DFRobot_DF1201S::playSpecFile(String str){
    if(curFunction != MUSIC) return false;
    sPacket_t cmd;
    cmd = pack("PLAYFILE",str);
+   pauseFlag = 1;
    writeATCommand(cmd.str,cmd.length);
    if(readAck() == "OK\r\n"){
     return true;
@@ -219,6 +220,7 @@ bool DFRobot_DF1201S::playFileNum(int16_t num){
    if(curFunction != MUSIC) return false;
    sPacket_t cmd;
    cmd = pack("PLAYNUM",String(num));
+   pauseFlag = 1;
    writeATCommand(cmd.str,cmd.length);
    if(readAck() == "OK\r\n"){
     return true;
@@ -501,7 +503,3 @@ String DFRobot_DF1201S::readAck(uint8_t len){
   }
   return str;
 }
-
-
-
-
